8c6a9c03e7bce4e4d2b596ced0adbade5c37376b
Hieu Hien Tran <hieuhien3012@Hieus-MacBook-Pro.local>
2018-06-04 23:13:09 +0700

    21321312

--- server.js
+++ server.js
@@ -50,15 +50,6 @@ app.post('/weblogin',urlencodedParser,function(req,res){
     })
 })
 
-app.get('/weblogout', function (req, res) {
-    res.clearCookie("teacher_ID")
-    .clearCookie("name")
-    .clearCookie("remember")
-    .clearCookie("student_ID")
-    .sendFile( __dirname + "/html/" + "login.html" );
-    userCookies[req.cookies.teacher_ID] = null;
-})
-
 app.get('/home', function (req, res) {
     var teacher_ID = req.cookies.teacher_ID
     if (teacher_ID != null) {
@@ -85,7 +76,6 @@ app.get('/class', function (req, res) {
 })
 
 app.get('/students', function (req, res) {
-    console.log(req.cookies)
     var teacher_ID = req.cookies.teacher_ID
     if (teacher_ID != null) {
         res.sendFile( __dirname + "/html/" + "students.html" );
@@ -97,6 +87,22 @@ app.get('/students', function (req, res) {
     }
 })
 
+app.get('/attendance',urlencodedParser,function(req,res){
+    // var student_ID = req.body.student_ID;
+    res.cookie("student_ID",student_ID)
+    .sendFile( __dirname + "/html/" + "attendance.html" );
+})
+
+app.get('/weblogout', function (req, res) {
+    res.clearCookie("teacher_ID")
+    .clearCookie("name")
+    .clearCookie("remember")
+    .clearCookie("student_ID")
+    .sendFile( __dirname + "/html/" + "login.html" );
+    userCookies[req.cookies.teacher_ID] = null;
+})
+
+//GET THINGS
 app.post('/getRooms',urlencodedParser,function (req,res) {
     console.log(req.body.teacher_ID)
     var cmd = "SELECT room_ID,major,minor FROM Rooms WHERE teacher_ID = "+req.body.teacher_ID
@@ -112,13 +118,13 @@ app.post('/getRooms',urlencodedParser,function (req,res) {
 })
 
 app.post('/getStudents',urlencodedParser,function (req,res) {
-    var cmd = "SELECT ST.student_ID AS student_ID, s.name AS name \
+    var sql = "SELECT ST.student_ID AS student_ID, s.name AS name \
         FROM Students_Teachers AS ST\
         JOIN Students AS s ON (ST.student_ID = s.student_ID) \
         JOIN Teachers AS t ON (ST.teacher_ID = t.teacher_ID) \
         WHERE ST.teacher_ID = ? \
         ORDER BY student_ID"
-        db.query(cmd,[parseInt(req.body.teacher_ID)],function(err,results){
+        db.query(sql,[parseInt(req.body.teacher_ID)],function(err,results){
             if (err) throw err;
             array = []
             for (var i = 0; i < results.length; i++) {
@@ -128,13 +134,49 @@ app.post('/getStudents',urlencodedParser,function (req,res) {
         });
 })
 
-app.post('/attendance',urlencodedParser,function(req,res){
-    var student_ID = req.body.student_ID;
-    res.cookie("student_ID",student_ID)
-    .end()
+app.post('/getAttendance',urlencodedParser,function (req,res){
+    var student_ID  =   parseInt(req.body.student_ID),
+        start       =   parseInt(req.body.start),
+        end         =   parserInt(req.body.end);
+    var sql = "SELECT a.time AS time, r.room_ID AS room \
+    FROM Attendance AS a\
+    JOIN Rooms AS r ON (a.major = r.major AND a.minor = r.minor)\
+    WHERE a.student_ID = ?\
+    AND a.time > ?\
+    AND a.time < ?\
+    ORDER BY time"
+    db.query(sql,[student_ID,start,end],function(err,results){
+        if (err) throw err;
+        var array = [];
+        var date    = new Date(results[0].time),
+            day     = date.getDay(),
+            start   = date.getTime(),
+            end     = date.getTime(),
+            room    = results[i].room;
+        for (var i = 1; i < results.length; i++) {
+            var d = new Date(results[i].time),
+                r = results[i].room;
+            if((d.getDay() == day) && (r == room) && ((d.getTime() - end) < 900000)){
+                end = results[i].time
+            } else {
+                if (end - start > 1800000) {
+                    var json = {
+                        day     : day,
+                        start   : timeFormat(start),
+                        end     : timeFormat(end),
+                        room    : room
+                    }
+                    array.push(json)
+                }
+                day     = d.getDay(), 
+                room    = r,
+                start   = d.getTime(),
+                end     = d.getTime() 
+            }
+        }
+        res.end(JSON.stringify(array))
+    });
 })
-
-
 
 app.post('/applogin',urlencodedParser,function(req,res){
     var student_ID = req.body.username;
@@ -211,95 +253,26 @@ app.post('/rssi',urlencodedParser,function(req,res){
     console.log(req.body)
     res.end();
 })
+
 app.get('/gethere',function(req,res){
     console.log(new Date())
     res.end();
 })
-// io.on('connection', function(socket) {
-
-//     socket.on("search",function(data){
-//         var cmd = "SELECT a.time AS time, r.room_ID AS room, s.name AS student, t.name AS teacher \
-//         FROM Attendance AS a \
-//         JOIN Rooms AS r ON (a.major = r.major AND a.minor = r.minor) \
-//         JOIN Students AS s ON (a.student_ID = s.student_ID) \
-//         JOIN Teachers AS t ON (r.teacher_ID = t.teacher_ID) \
-//         WHERE a.student_ID = ? \
-//         ORDER BY time"
-//         db.query(cmd,[parseInt(data)],function(err,results){
-//             if (err) throw err;
-//             var room = "";
-//             var teacher = "";
-//             var json = {
-//                 start: "2010-01-01T00:00:00",
-//                 end: "2010-01-01T00:00:00",
-//                 text: ""
-//             };
-            
-//             for (var i = 0; i < array.length; i++) {
-//                 var t = parseInt(results[i].time);
-//                 t = new Date(t)
-//                 var s = new Date(json.start)
-//                 var e = new Date(json.end)
-//                 var a = dateCompare(t,s);               //Comparing Date
-//                 var b = (results[i].room == room);      //Comparing Room
-//                 var c = timeCompare(t,e);               //Comparing Time
-//                 if (a && b && c) {
-//                     json.end = timeFormat(t);
-//                     var j = parseInt(i)+1;
-//                     if(results[j] == null){
-//                         socket.emit('attendance',json);
-//                     }
-//                 } else {
-//                     socket.emit('attendance',json);
-//                     json.start = timeFormat(t);
-//                     json.end = json.start;
-//                     room = results[i].room;
-//                     teacher = results[i].teacher;
-//                     json.text = "Room: " + room + " Teacher: " + teacher;
-//                 } 
-                
-//             }
-//         });
-//     })
-    
-//     socket.on('disconnect', function () {
-//     //    console.log('A user disconnected');
-//     });
-// });
 
 http.listen(8800, function() {
     console.log('listening on *:8800');
     console.log('cookies: ',userCookies)
 });
 
-function dateCompare(t,s) {
-    var ret = (t.getFullYear() == s.getFullYear()) && (t.getMonth() == s.getMonth()) && (t.getDate() == s.getDate());
-    return ret;
-}
-
-function timeCompare(t,e) {
-    var a = t.getHours() == e.getHours();
-    var b = (t.getMinutes() - e.getMinutes()) <= 10;
-    return (a && b);
-}
-
 function timeFormat(t){
     var ret = new Date(t)
-    
-    var year = ret.getFullYear();
-    year = year.toString();
-    var month = ret.getMonth()+1;
-    month =  month < 10 ? '0'+ month: month.toString();
-    var date = ret.getDate();
-    date =  date < 10 ? '0'+ date: date.toString();
+
     var hours = ret.getHours();
     hours =  hours < 10 ? '0'+ hours: hours.toString();
     var minutes = ret.getMinutes();
     minutes =  minutes < 10 ? '0'+ minutes: minutes.toString();
-    var seconds = ret.getSeconds();
-    seconds =  seconds < 10 ? '0'+ seconds: seconds.toString();
 
-    ret = year+"-"+month+"-"+date+"T"+hours+":"+minutes+":"+seconds;
+    ret = hours+":"+minutes;
     return ret;
 }
 
