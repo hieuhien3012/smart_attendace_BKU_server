5e85fdd7241740afd77a5178e0dce683c3181f86
Hieu Hien Tran <hieuhien3012@Hieus-MacBook-Pro.local>
2018-06-27 10:43:34 +0700

    321321321321321

--- server.js
+++ server.js
@@ -255,8 +255,8 @@ app.post('/getAttendance',urlencodedParser,function (req,res){
             res.end(JSON.stringify({
                 span : JSON.stringify(dateArray),
                 event: JSON.stringify(array),
-                id   : result[0].id,
-                name : result[0].name
+                id   : results[0].id,
+                name : results[0].name
             }))
         }
     });
