3fdda171f3c16979bc03f06ce6a690b9655a93d1
Hieu Hien Tran <hieuhien3012@Hieus-MacBook-Pro.local>
2018-06-27 09:05:37 +0700

    221321312

--- server.js
+++ server.js
@@ -245,7 +245,6 @@ app.post('/getAttendance',urlencodedParser,function (req,res){
             for (var i = 1; i < results.length; i++) {
                 var d = new Date(parseInt(results[i].time)),
                     r = results[i].room;
-                    console.log(d.getDay(),r,d.getTime(),end)
                 if((d.getDay() == day) && (r == room) && ((d.getTime() - end) < 900000)){
                     end = parseInt(results[i].time)
                     if (results[i+1] == null,end - start > 1800000) {
